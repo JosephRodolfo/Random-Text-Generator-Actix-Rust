a3c3403d75cbe54e313708af125f3ab22be49a53
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-07-19 15:53:16 -0400

    Add initial full article text route and remove word list files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 // use actix_web::http::header::Date;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
-use censor::*;
 use chrono::{DateTime, NaiveDateTime, Utc};
 use rand::prelude::{IteratorRandom, SliceRandom};
 use rand::{thread_rng, Rng};
@@ -13,6 +12,11 @@ struct ArticleTitle {
     title: String,
     subtitle: String,
     date: String,
+    text: String,
+}
+#[derive(Debug, Serialize)]
+struct RandomWord {
+    word: String,
 }
 
 fn create_random_date() -> DateTime<Utc> {
@@ -30,51 +34,46 @@ fn generate_random_number(min: u32, max: u32) -> u32 {
     return number;
 }
 
-// fn get_line_count(length: u8) -> Option<u16> {
-//     let total_lines = match length {
-//         0 => {
-//             let random = generate_random_number(3, 15);
-//             return Some(random);
-//         }
-
-//         3 => return Some(1013),
-//         4 => return Some(4029),
-//         5 => return Some(8937),
-//         6 => return Some(15787),
-//         7 => return Some(24028),
-//         8 => return Some(29765),
-//         9 => return Some(29149),
-//         10 => return Some(22325),
-//         11 => return Some(16164),
-//         12 => return Some(11416),
-//         13 => return Some(7750),
-//         14 => return Some(5058),
-//         15 => return Some(3155),
-
-//         _ => None,
-//     };
-
-//     return total_lines;
-// }
-
-fn get_random_word(number: u8) -> String {
+fn get_random_word(number: u8) -> RandomWord {
     let path = format!("src/random_words/{}-letters.txt", number.to_string());
     let rand_word = read_file_line_by_line(&path);
     let single_word = &rand_word[0];
-    return single_word.to_string();
+
+    let word_obj = RandomWord {
+        word: single_word.to_string(),
+    };
+    return word_obj;
 }
 
-// #[get("/word/{number}")]
 fn get_word(req: HttpRequest) -> HttpResponse {
     // HttpResponse::Ok().content_type("text/html").body("Test")
     let num_letters: u8 = req.match_info().get("number").unwrap().parse().unwrap();
     let word = get_random_word(num_letters);
-    HttpResponse::Ok().json(format!("word: {}", word))
+    HttpResponse::Ok().json(word)
 }
 
-fn get_article() -> HttpResponse {
-    let article_obj = get_title_subtitle();
-    return HttpResponse::Ok().json(article_obj);
+fn get_article(req: HttpRequest) -> HttpResponse {
+    let mut articles: Vec<ArticleTitle> = Vec::new();
+    let num_articles: u8 = req.match_info().get("number").unwrap().parse().unwrap();
+
+    for _i in 0..num_articles {
+        let article_obj = get_title_subtitle();
+        articles.push(article_obj);
+    }
+    return HttpResponse::Ok().json(articles);
+}
+
+fn get_fulltext(req: HttpRequest) -> HttpResponse {
+    let mut articles: Vec<ArticleTitle> = Vec::new();
+    let num_articles: u8 = req.match_info().get("number").unwrap().parse().unwrap();
+
+    for _i in 0..num_articles {
+        let mut article_obj = get_title_subtitle();
+        let text = create_paragraph();
+        article_obj.text = text;
+        articles.push(article_obj);
+    }
+    return HttpResponse::Ok().json(articles);
 }
 
 fn some_kind_of_uppercase_first_letter(s: &str) -> String {
@@ -85,38 +84,47 @@ fn some_kind_of_uppercase_first_letter(s: &str) -> String {
     }
 }
 
-fn get_title_subtitle() -> ArticleTitle {
-    fn create_title() -> Vec<String> {
-        let mut vec: Vec<String> = Vec::new();
+fn create_title(title: String) -> String {
+    let mut vec: Vec<String> = Vec::new();
 
-        for i in 3..15 {
-            let path = format!("src/random_words/{}-letters.txt", i.to_string());
+    for i in 3..15 {
+        let path = format!("src/random_words/{}-letters.txt", i.to_string());
 
-            let mut result = read_file_line_by_line(&path);
-            vec.append(&mut result);
-        }
-        vec.shuffle(&mut thread_rng());
-        return vec;
+        let mut result = read_file_line_by_line(&path);
+        vec.append(&mut result);
     }
-
-    let title = create_title();
-    let mut subtitle = create_title();
-
-    let uppper_case_title = title
-        .iter()
-        .map(|e| some_kind_of_uppercase_first_letter(e))
-        .collect::<Vec<String>>();
-    subtitle[0] = some_kind_of_uppercase_first_letter(&subtitle[0]);
-    let joined_chars_title = &uppper_case_title.join(" ");
-    let joined_chars_subtitle = &subtitle.join(" ");
-    let date = create_random_date().to_string();
-
-    let article = ArticleTitle {
-        title: joined_chars_title.to_string(),
-        subtitle: joined_chars_subtitle.to_string(),
-        date,
+    vec.shuffle(&mut thread_rng());
+    let mut new_string = vec;
+    let final_tital = match title.as_str() {
+        "title" => {
+            let result = new_string
+                .iter()
+                .map(|e| some_kind_of_uppercase_first_letter(e))
+                .collect::<Vec<String>>()
+                .join(" ");
+            return result;
+        }
+        "subtitle" => {
+            new_string[0] = some_kind_of_uppercase_first_letter(&new_string[0]);
+            let result = new_string.join(" ");
+            return result;
+        }
+        _ => "",
     };
-    println!("{:#?}", article);
+
+    return final_tital.to_string();
+}
+
+fn get_title_subtitle() -> ArticleTitle {
+    let title = create_title("title".to_string());
+    let subtitle = create_title("subtitle".to_string());
+    let date = create_random_date().to_string();
+    let article = ArticleTitle {
+        title,
+        subtitle,
+        date,
+        text: "".to_string(),
+    };
     return article;
 }
 
@@ -127,28 +135,53 @@ fn read_file_line_by_line(filepath: &str) -> Vec<String> {
     let reader = BufReader::new(&file);
     let lines = reader.lines().map(|l| l.expect("Couldn't read line"));
 
-    let mut random = lines
+    let random = lines
         .choose(&mut rand::thread_rng())
         .expect("File had no lines");
-    let censor = Standard + Zealous + Sex;
 
-    while censor.check(&random) {
+    vec.push(random);
+
+    return vec;
+}
+
+fn create_paragraph() -> String {
+    let mut random = Vec::new();
+
+    for i in 3..15 {
+        let path = format!("src/random_words/{}-letters.txt", i.to_string());
+
+        let path = Path::new(&path);
+        let file = File::open(path).expect("Cannot open file.txt");
         let reader = BufReader::new(&file);
         let lines = reader.lines().map(|l| l.expect("Couldn't read line"));
-
-        random = lines
-            .choose(&mut rand::thread_rng())
-            .expect("File had no lines");
+        let mut new_words = lines.choose_multiple(&mut rand::thread_rng(), 100);
+        random.append(&mut new_words);
     }
-    vec.push(random);
-    return vec;
+
+    random.shuffle(&mut thread_rng());
+    let mut new_arr = random;
+    let length = new_arr.len();
+    new_arr[0] = some_kind_of_uppercase_first_letter(&new_arr[0]);
+    new_arr[length - 1] = format!("{}.", new_arr[length - 1]);
+
+    for i in 10..length {
+        if i % 15 == 0 {
+            new_arr[i - 1] = format!("{}.", new_arr[i - 1]);
+            new_arr[i] = some_kind_of_uppercase_first_letter(&new_arr[i]);
+        }
+    }
+
+    let para_string = new_arr.join(" ");
+    println!("{}", para_string);
+    return para_string;
 }
 
 fn main() {
     let server = HttpServer::new(|| {
         App::new()
             .route("/word/{number}", web::get().to(get_word))
-            .route("/article", web::get().to(get_article))
+            .route("/article/{number}", web::get().to(get_article))
+            .route("/fulltext/{number}", web::get().to(get_fulltext))
     });
     println!("Serving on http://localhost:3000...");
     server
